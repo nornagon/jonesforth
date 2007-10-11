f8688e0ca6ee2928ae1441ba2b7f5d2c8f43dea5
rich <rich>
2007-10-11 07:41:51 +0000

    Test assembler & INLINE

---
+++ test_assembler.f
@@ -0,0 +1,19 @@
+( -*- text -*- )
+
+: 2DROP INLINE DROP INLINE DROP ;CODE
+
+: C@++ INLINE DUP INLINE 1+ INLINE SWAP INLINE C@ ;CODE
+
+: TEST
+	." 2DROP: "	1 2 3 4 2DROP . . CR
+
+	S" testing" DROP
+	C@++ EMIT CR
+	C@++ EMIT CR
+	C@++ EMIT CR
+	C@++ EMIT CR
+	C@++ EMIT CR
+	C@++ EMIT CR
+	C@++ EMIT CR
+	DROP
+;
